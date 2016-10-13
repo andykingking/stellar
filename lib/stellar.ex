8b689a786d9c051cde3f9afeccfbd9964aca3789
Andrew King <me@andrewsking.com>
2016-10-13 14:19:08 +1100

    Allow passing in background option

--- lib/stellar.ex
+++ lib/stellar.ex
@@ -7,14 +7,27 @@ defmodule Stellar do
   alias NASA.ImageOfTheDay
 
   @logo %Image{path: "assets/MYOB_logo_mono.png"}
+  @aliases [
+    b: :background
+  ]
+  @options [
+    background: :string
+  ]
 
   @doc """
   The main function for the application.
   """
   def main(args) do
     Logger.debug("Application started")
+    parsed_args = OptionParser.parse(args, aliases: @aliases, strict: @options)
 
-    resulting_image_path = args |> List.first
+    resulting_image_path = elem(parsed_args, 1) |> List.first
+
+    background =
+      parsed_args
+      |> elem(0)
+      |> Keyword.get(:background)
+      |> fetch_background
 
     @logo
     |> Composite.compose(background)
@@ -25,11 +38,12 @@ defmodule Stellar do
     Logger.debug("Application complete")
   end
 
-  defp background do
-    image_url =
-      ImageOfTheDay.get
-      |> Map.get(:hdurl)
-
+  defp fetch_background(image_url) when is_nil(image_url) do
+    ImageOfTheDay.get
+    |> Map.get(:hdurl)
+    |> fetch_background
+  end
+  defp fetch_background(image_url) do
     extension = Path.extname(image_url)
 
     image_url
